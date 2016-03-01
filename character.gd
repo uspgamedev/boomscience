b08d819a3f835caf2455d468ba7fb735ecb22e3e
orenjiakira <orenjiakira@gmail.com>
2016-03-01 17:48:13 -0300

    enforced 60fps loop

--- character.gd
+++ character.gd
@@ -1,6 +1,9 @@
 
 extends RigidBody2D
 
+var FRAMERATE = 60
+var dtotal = 0
+
 var pos_x
 var pos_y
 
@@ -8,14 +11,18 @@ func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	pos_x = get_pos().x
-	pos_y = get_pos().y
-	if (Input.is_key_pressed(KEY_RIGHT)):
-		self.set_pos(Vector2(pos_x + delta*3*60, pos_y))
-	if (Input.is_key_pressed(KEY_LEFT)):
-		self.set_pos(Vector2(pos_x - delta*3*60, pos_y))
-	if (Input.is_key_pressed(KEY_UP)):
-		self.set_linear_velocity(Vector2(0, -100))
-		
+	dtotal += delta
+	while dtotal >= 1/FRAMERATE:
+		dtotal -= delta
+		pos_x = get_pos().x
+		pos_y = get_pos().y
+		if (Input.is_key_pressed(KEY_RIGHT)):
+			print("right")
+			self.set_pos(Vector2(pos_x + 3, pos_y))
+		if (Input.is_key_pressed(KEY_LEFT)):
+			print("left")
+			self.set_pos(Vector2(pos_x - 3, pos_y))
+		if (Input.is_key_pressed(KEY_UP)):
+			self.set_linear_velocity(Vector2(0, -100))
 
 
