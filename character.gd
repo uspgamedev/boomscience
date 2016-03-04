e5cffae4929f401cb52ad5974e6a6bd31c0e86ee
Kazuo256 <kazuo256@gmail.com>
2016-03-04 13:59:47 -0300

    Fixed process
    Removed FPS control since it is not necessary

--- character.gd
+++ character.gd
@@ -1,9 +1,6 @@
 
 extends RigidBody2D
 
-var FRAMERATE = 60
-var dtotal = 0
-
 var pos_x
 var pos_y
 
@@ -11,18 +8,15 @@ func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	dtotal += delta
-	while dtotal >= 1/FRAMERATE:
-		dtotal -= delta
-		pos_x = get_pos().x
-		pos_y = get_pos().y
-		if (Input.is_key_pressed(KEY_RIGHT)):
-			print("right")
-			self.set_pos(Vector2(pos_x + 3, pos_y))
-		if (Input.is_key_pressed(KEY_LEFT)):
-			print("left")
-			self.set_pos(Vector2(pos_x - 3, pos_y))
-		if (Input.is_key_pressed(KEY_UP)):
-			self.set_linear_velocity(Vector2(0, -100))
+	pos_x = get_pos().x
+	pos_y = get_pos().y
+	if (Input.is_key_pressed(KEY_RIGHT)):
+		print("right")
+		self.set_pos(Vector2(pos_x + 3, pos_y))
+	if (Input.is_key_pressed(KEY_LEFT)):
+		print("left")
+		self.set_pos(Vector2(pos_x - 3, pos_y))
+	if (Input.is_key_pressed(KEY_UP)):
+		self.set_linear_velocity(Vector2(0, -100))
 
 
