99b48afbb2ce9e1d740f5f75eb97f1ec5d577ea7
Victor Domiciano <victor.dm3@gmail.com>
2016-03-04 14:54:08 -0300

    Fixed jumping, created throw button.

--- character.gd
+++ character.gd
@@ -4,19 +4,26 @@ extends RigidBody2D
 var pos_x
 var pos_y
 
+var is_jumping = true
+
 func _ready():
 	set_fixed_process(true)
+	set_process_input(true)
 	
 func _fixed_process(delta):
 	pos_x = get_pos().x
 	pos_y = get_pos().y
 	if (Input.is_key_pressed(KEY_RIGHT)):
-		print("right")
 		self.set_pos(Vector2(pos_x + 3, pos_y))
 	if (Input.is_key_pressed(KEY_LEFT)):
-		print("left")
 		self.set_pos(Vector2(pos_x - 3, pos_y))
-	if (Input.is_key_pressed(KEY_UP)):
-		self.set_linear_velocity(Vector2(0, -100))
+	if (Input.is_key_pressed(KEY_UP) and is_jumping == false):
+		is_jumping = true
+		self.set_linear_velocity(Vector2(0, -300))
 
-
+func _on_RigidBody2D_body_enter( body ):
+	is_jumping = false
+	
+func _input(event):
+	if(event.is_action_pressed("throw")):
+		print("kaboom")
