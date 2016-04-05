e0852c44c0ffdbc9ce6be70b3c76be6e2f0241c9
Victor Domiciano <victor.dm3@gmail.com>
2016-04-05 16:26:23 -0300

    Limit enemy movement to platforms
    Enemies don't fall.

--- enemy.gd
+++ enemy.gd
@@ -9,13 +9,16 @@ var count = 0
 var max_count = 5
 var next_tile
 var ray_cast
+var ray_cast2
 var sprite
 
 func _ready():
 	set_fixed_process(true)
 	ray_cast = get_node("RayCast2D")
+	ray_cast2 = get_node("RayCast2D 2")
 	sprite = get_node("enemy")
 	ray_cast.add_exception(self)
+	ray_cast2.add_exception(self)
 
 func _fixed_process(delta):
 	if direction == 1:
@@ -32,6 +35,10 @@ func _fixed_process(delta):
 	if (ray_cast.is_colliding()):
 		direction *= -1
 		ray_cast.set_scale(Vector2(ray_cast.get_scale().x * -1, 1))
+		count = 0
+	if (!ray_cast2.is_colliding()):
+		direction *= -1
+		ray_cast2.set_pos(Vector2(ray_cast2.get_pos().x * -1, ray_cast2.get_pos().y))
 		count = 0
 
 func bomb_collision():
