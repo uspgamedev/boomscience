02452c20d6c28a1207938a592fa973cfdff58bc7
andrevic <name_andrevsn@hotmail.com>
2016-09-21 18:05:53 -0300

    Add jumping flag

--- scripts/body.gd
+++ scripts/body.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends KinematicBody2D	
 
 const DIR = preload("directions.gd")
 const G = 4000 # Gravity
@@ -10,6 +10,7 @@ var hp # Health points
 var normal # Normal force, perpendicular to the surface
 var motion # Displacement
 var directions = DIR.new()
+var can_jump = false
 
 func _ready():
 	set_fixed_process(true)
@@ -23,11 +24,18 @@ func _add_speed(dir):
 	#self.speed += directions.get_dir(dir) * ACC
 	self.speed += DIR.VECTOR[dir] * ACC
 
+func check_if_floor(collider, normal):
+	if collider extends StaticBody2D:
+		var angle = atan2( normal.x, -normal.y )
+		if angle > PI/4 and angle < 3*PI/4:
+			can_jump = true
+
 func apply_speed(delta):
 	var motion = move(self.speed * delta)
 	if is_colliding():
 		var collider = get_collider()
 		var normal = get_collision_normal()
+		check_if_floor(collider, normal)
 		motion = .01 * normal.slide(self.speed)
 		move(motion)
 
@@ -35,8 +43,10 @@ func apply_gravity(delta):
 	speed.y += delta * G
 
 func jump(dir):
+	if !can_jump: return
 	if dir == DIR.UP or dir == DIR.UP_LEFT or dir == DIR.UP_RIGHT:
 		speed -= Vector2(0, 0.1 * G)
+		can_jump = false
 
 func deaccelerate():
 	if (speed.length_squared() < EPSILON):
