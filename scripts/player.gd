4de5f694a887556d0be2154807db6328cc72e31c
andrevic <name_andrevsn@hotmail.com>
2016-09-21 15:49:27 -0300

    Change movement input to use connection

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,14 +5,16 @@ const DIR = preload("directions.gd")
 const ACC = 3
 
 var _ref_dirs = DIR.new()
-onready var input = get_node("/root/input")
 
-func ready():
+func _ready():
 	set_fixed_process(true)
+	get_node("/root/input").connect('hold_direction', self, '_on_hold_direction')
 
 func _fixed_process(delta):
-	self._apply_speed(delta)
+	pass
 
-func _apply_speed(delta):
-	if (input.dir >= 0):
-		self.v += _ref_dirs.get_dir(input.dir) * ACC
+func _on_hold_direction(dir):
+	_apply_speed(_ref_dirs.get_dir(dir))
+
+func _apply_speed(dir):
+	self.v += dir * ACC
