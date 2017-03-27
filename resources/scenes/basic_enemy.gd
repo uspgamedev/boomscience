83a1dc4248526f878ad98f69288d6b08c4cedfe2
Victor Domiciano <victor.dm3@gmail.com>
2017-03-27 12:12:51 -0300

    Add routine to enemy
    Signed off by: @iblucher
    Signed off by: @Kazuo256

--- resources/scenes/basic_enemy.gd
+++ resources/scenes/basic_enemy.gd
@@ -1,2 +1,33 @@
 extends 'res://scripts/body.gd'
 
+var anim = 'idle'
+var anim_new
+var max_speed = 100
+var dir = -1
+var timer = 2
+var temp = 0
+
+func _ready():
+	sprite = get_node('BasicEnemySprite')
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	check_animation()
+	routine(delta)
+	
+func check_animation():
+	if (!speed.x):
+		anim_new = 'idle'
+	else:
+		anim_new = 'walk'
+	if (anim != anim_new):
+		anim = anim_new
+		get_node('BasicEnemySprite/BasicEnemyAnimation').play(anim)
+	
+func routine(delta):
+	speed.x = dir * max_speed
+	temp += delta
+	if (temp >= timer):
+		temp = 0
+		dir *= -1
+		sprite.set_flip_h(max(dir, 0))
