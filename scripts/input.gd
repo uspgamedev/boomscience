1f944b38c6c415cf1ae13e3728e871c30d8b435b
orenjiakira <orenjiakira@gmail.com>
2017-05-31 17:05:48 -0300

    Fix input module

--- scripts/input.gd
+++ scripts/input.gd
@@ -1,40 +1,139 @@
+
 extends Node
 
 const DIR = preload('directions.gd')
 const ACT = preload('actions.gd')
+const INVALID = -1
 
 signal hold_direction(dir)
 signal hold_action(act)
 signal press_direction(dir)
 signal press_action(act)
+signal release_direction(dir)
+signal release_action(dir)
+
+# WEIRD AS FUCK HARDCODED STUFF WHY WOULD YOU DO THIS
 signal press_quit
 signal press_reset
 signal press_respawn
 signal press_bomb_throw
-
-var dir = -1
 
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
 
 func _input(event):
-	dir = self._get_direction(event)
-	var act = self._get_action(event)
-	var throw = self._get_throw(event)
-	if dir != -1: emit_signal('press_direction', dir)
-	if act != -1: emit_signal('press_action', act)
-	if throw != -1: emit_signal('press_bomb_throw', throw)
-	if act != -1: emit_signal('press_respawn', act)
+	var pressed_dir = _get_direction(event, "pressed")
+	var pressed_act = _get_action(event, "pressed")
+	var released_dir = _get_direction(event, "released")
+	var released_act = _get_action(event, "released")
+	if pressed_dir != INVALID: emit_signal('press_direction', pressed_dir)
+	if pressed_act != INVALID: emit_signal('press_action', pressed_act)
+	if released_dir != INVALID: emit_signal('release_action', released_dir)
+	if released_act != INVALID: emit_signal('release_action', released_act)
+	do_your_weird_thing(event, pressed_act)
+
+func _fixed_process(delta):
+	var held_dir = _get_direction(Input, "held")
+	var held_act = _get_action(Input, "held")
+	if held_dir != INVALID: emit_signal('hold_direction', held_dir)
+	if held_act != INVALID: emit_signal('hold_action', held_act)
+
+func _get_action(e, interaction):
+	var act = INVALID
+	var check_action = FuncRef.new()
+
+	if not interaction: return
+
+	# configure funcref's target object and method
+	check_action.set_instance(e)
+	if interaction == "pressed":
+		check_action.set_function("is_action_pressed")
+	elif interaction == "released":
+		check_action.set_function("is_action_released")
+	elif interaction == "held":
+		check_action.set_function("is_action_pressed")
+	else:
+		return
+
+	# check input with set interaction
+	if check_action.call_func('ui_accept'):
+		act = ACT.ACCEPT
+	if check_action.call_func('ui_cancel'):
+		act = ACT.CANCEL
+	if check_action.call_func('ui_camera'):
+		act = ACT.CAMERA
+	if check_action.call_func('ui_stealth'):
+		act = ACT.STEALTH
+	if check_action.call_func('ui_jump'):
+		act = ACT.JUMP
+	if check_action.call_func('ui_instructions'):
+		act = ACT.INST
+	if check_action.call_func('ui_interact'):
+		act = ACT.INTERACT
+	if check_action.call_func('ui_switch'):
+		act = ACT.SWITCH
+
+	return act
+
+func _get_throw(e):
+	var throw = -1
+	if e.is_action_pressed('ui_throw'):
+		throw = ACT.THROW
+	return throw
+
+func _get_direction(e, interaction):
+	var dir = INVALID
+	var check_action = FuncRef.new()
+
+	if not interaction: return
+
+	# configure funcref's target object and method
+	check_action.set_instance(e)
+	if interaction == "pressed":
+		check_action.set_function("is_action_pressed")
+		print("pressed!")
+	elif interaction == "released":
+		check_action.set_function("is_action_released")
+		print("released!")
+	elif interaction == "held":
+		check_action.set_function("is_action_pressed")
+		print(e.is_action_pressed("ui_down"))
+		print("held")
+	else:
+		return
+
+	# check input with set interaction
+	if check_action.call_func('ui_up') and not check_action.call_func('ui_down'):
+		dir = DIR.UP
+	elif check_action.call_func('ui_down') and not check_action.call_func('ui_up'):
+		dir = DIR.DOWN
+	if check_action.call_func('ui_right') and not check_action.call_func('ui_left'):
+		dir = DIR.RIGHT
+	elif check_action.call_func('ui_left') and not check_action.call_func('ui_right'):
+		dir = DIR.LEFT
+	if check_action.call_func('ui_up') and check_action.call_func('ui_right') \
+		and not check_action.call_func('ui_down') and not check_action.call_func('ui_left'):
+		dir = DIR.UP_RIGHT
+	elif check_action.call_func('ui_down') and check_action.call_func('ui_left') \
+		and not check_action.call_func('ui_up') and not check_action.call_func('ui_right'):
+		dir = DIR.DOWN_LEFT
+	if check_action.call_func('ui_down') and check_action.call_func('ui_right') \
+		and not check_action.call_func('ui_up') and not check_action.call_func('ui_left'):
+		dir = DIR.DOWN_RIGHT
+	elif check_action.call_func('ui_up') and check_action.call_func('ui_left') \
+		and not check_action.call_func('ui_down') and not check_action.call_func('ui_right'):
+		dir = DIR.UP_LEFT
+
+	return dir
+
+func do_your_weird_thing(event, act):
+	var throw = _get_throw(event)
+	if throw != INVALID: emit_signal('press_bomb_throw', throw)
+	if act != INVALID: emit_signal('press_respawn', act)
 	if _get_quit(event): emit_signal('press_quit')
 	if _get_reset(event): emit_signal('press_reset')
 	if _get_respawn(event): emit_signal('press_respawn')
-
-func _fixed_process(delta):
-	dir = self._get_direction(Input)
-	var act = self._get_action(Input)
-	if dir != -1: emit_signal('hold_direction', dir)
-	if act != -1: emit_signal('hold_action', act)
 
 func _get_reset(e):
 	if e.is_action_pressed('ui_reset'):
@@ -48,52 +147,3 @@ func _get_quit(e):
 	if e.is_action_pressed('ui_quit'):
 		return true
 
-func _get_action(e):
-	var act = -1
-	if e.is_action_pressed('ui_accept'):
-		act = ACT.ACCEPT
-	if e.is_action_pressed('ui_cancel'):
-		act = ACT.CANCEL
-	if e.is_action_pressed('ui_camera'):
-		act = ACT.CAMERA
-	if e.is_action_pressed('ui_stealth'):
-		act = ACT.STEALTH
-	if e.is_action_pressed('ui_jump'):
-		act = ACT.JUMP
-	if e.is_action_pressed('ui_instructions'):
-		act = ACT.INST
-	if e.is_action_pressed('ui_interact'):
-		act = ACT.INTERACT
-	if e.is_action_pressed('ui_switch'):
-		act = ACT.SWITCH
-	return act
-
-func _get_throw(e):
-	var throw = -1
-	if e.is_action_pressed('ui_throw'):
-		throw = ACT.THROW
-	return throw
-
-func _get_direction(e):
-	dir = -1
-	if e.is_action_pressed('ui_up') and not e.is_action_pressed('ui_down'):
-		dir = DIR.UP
-	elif e.is_action_pressed('ui_down') and not e.is_action_pressed('ui_up'):
-		dir = DIR.DOWN
-	if e.is_action_pressed('ui_right') and not e.is_action_pressed('ui_left'):
-		dir = DIR.RIGHT
-	elif e.is_action_pressed('ui_left') and not e.is_action_pressed('ui_right'):
-		dir = DIR.LEFT
-	if e.is_action_pressed('ui_up') and e.is_action_pressed('ui_right') \
-		and not e.is_action_pressed('ui_down') and not e.is_action_pressed('ui_left'):
-		dir = DIR.UP_RIGHT
-	elif e.is_action_pressed('ui_down') and e.is_action_pressed('ui_left') \
-		and not e.is_action_pressed('ui_up') and not e.is_action_pressed('ui_right'):
-		dir = DIR.DOWN_LEFT
-	if e.is_action_pressed('ui_down') and e.is_action_pressed('ui_right') \
-		and not e.is_action_pressed('ui_up') and not e.is_action_pressed('ui_left'):
-		dir = DIR.DOWN_RIGHT
-	elif e.is_action_pressed('ui_up') and e.is_action_pressed('ui_left') \
-		and not e.is_action_pressed('ui_down') and not e.is_action_pressed('ui_right'):
-		dir = DIR.UP_LEFT
-	return dir
