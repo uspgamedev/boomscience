366d3cf71d85b17997a60c46f622a921de2fbf2b
orenjiakira <orenjiakira@gmail.com>
2017-06-02 13:33:14 -0300

    Add theoretical double mapping for actions
    Created enum in `buttons.gd` that enumerate keys to actions in two
    different control styles: one is fps-like, and the other is full keyboard.

---
+++ scripts/buttons.gd
@@ -0,0 +1,31 @@
+
+extends Node
+
+enum CONSOLE_LIKE {
+	KEY_Z,   # yes
+	KEY_X,   # no
+	KEY_C,   # attack
+	SPACE,   # jump
+	CONTROL, # camera
+	KEY_F,   # toggle change prev
+	KEY_G,   # toggle change next
+	ESCAPE,  # pause
+	F1,      # help
+	F2,      # debug respawn
+	F3,      # debug reset
+}
+
+enum FPS_LIKE {
+	KEY_E,   # yes
+	CLICK_2, # no
+	CLICK_1, # attack
+	SPACE,   # jump
+	CONTROL, # camera
+	KEY_Q,   # toggle change prev
+	SHIFT,   # toggle change next
+	ESCAPE,  # pause
+	F1,      # help
+	F2,      # debug respawn
+	F3,      # debug reset
+}
+
