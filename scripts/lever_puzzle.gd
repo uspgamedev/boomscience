38512788d541d77916fd52f514af8e19d5cc725b
Kazuo256 <kazuo256@gmail.com>
2017-07-04 10:05:30 -0300

    Change lever gate tile

--- scripts/lever_puzzle.gd
+++ scripts/lever_puzzle.gd
@@ -1,23 +1,24 @@
 extends TileMap
 
 const EMPTY = -1
-const DOOR = 14
+const DOOR = 52
 
 var is_door_moving = false
 
 onready var global = get_node('/root/global')
+onready var gates = get_node('Gates')
 
 func _ready():
 	door_interaction()
 
 func door_interaction():
 	is_door_moving = true
-	for i in range (-2, 2):
+	for i in range (-3, 3):
 		for k in range (0, 5):
 			yield(get_tree(), 'fixed_frame')
 		for j in range (0, 5):
 			if (global.flags['door'][j] == global.OPEN):
-				set_cell(20 + 2*j, -i-1, EMPTY)
+				gates.set_cell(20 + 2*j, -i-1, EMPTY)
 			elif (global.flags['door'][j] == global.CLOSED):
-				set_cell(20 + 2*j, i, DOOR)
+				gates.set_cell(20 + 2*j, i, DOOR)
 	is_door_moving = false
